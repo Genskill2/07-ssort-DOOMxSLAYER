13bc58401e6fef471babad5b1e28d40c40397955
DOOMxSLAYER <84665718+DOOMxSLAYER@users.noreply.github.com>
2021-06-26 16:40:10 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,11 +1,32 @@
 void swap_max(int a[], int b, int c)
 {
-  int i, d;
+  int i, d, s;
+  if(c==b-2)
+  {
+  	if(a[c]<a[b-1])
+  	{
+  		int f=a[c];
+  		a[c]=a[b-1];
+  		a[b-1]=f;
+  	}
+  }
+  else
+  {		
   d=a[c];
-  for(i=c;i-1<b;i++)
+  for(i=c;i<b-2;i++)
   {
     if (d<a[i+1])
+      s=i+1;
       d=a[i+1];
    }
+   a[s]=a[c];
    a[c]=d;
+   }
  }
+
+
+void ssort(int m[], int n)
+{
+  for(int j=0;j<n;j++)
+    swap_max(m,n,j);
+}
